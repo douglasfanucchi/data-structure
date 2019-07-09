13553ddc127c5ccf1aa9df93aefee3c796fe3863
Douglas Fanucchi <douglas@fanucchi.dev>
2019-07-09 12:39:36 -0300

    print list method implemented

--- list.c
+++ list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void main() {
 	#define MAX 50
@@ -19,5 +20,14 @@ void main() {
 	int elementsCount(List* l) {
         	return l->qtElements;
 	}
-  
+
+	bool printList(List* l) {
+		if( elementsCount(l) < 0 || elementsCount(l) > MAX )
+			return false;	
+
+		for( int i = 0; i < l->qtElements; i++ ) 
+			printf("Index -> %d \n", l->elements[i].index);
+
+		return true;
+	}
 }
